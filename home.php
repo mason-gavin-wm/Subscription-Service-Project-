f31594cd5dd5c507be08d6e61d460363f9bed0a9
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-04-11 15:24:53 -0700

    First Commit small changing

---
+++ home.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 4/11/16
+ * Time: 2:40 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<!--NAV START-->
+<div class="nav">
+    <div class="container">
+        <ul class="pull-left">
+            <li><a href="#">Packages</a></li>
+            <li><a href="#">Customize Your Own</a></li>
+        </ul>
+        <ul class="pull-right">
+            <li><a href="#">FAQS</a></li>
+            <li><a href="#">Log In</a></li>
+            <li><a href="#">My Account</a></li>
+        </ul>
+    </div>
+</div>
+<!--NAV END-->
+
+<!--JUMBOTRON START-->
+<div class="jumbotron">
+    <div class="container">
+        <h1>Rent from The Mr. Club</h1>
+        <p>Personalized style for everyone.</p>
+        <p>Rent unlimited styles hand-selected just for you.</p>
+        <a href="#">BECOME A MEMBER</a>
+    </div>
+</div>
+<!--JUMBOTRON END-->
+
+<div class="neighborhood-guides">
+    <div class="container">
+        <h2>Gallery of Animals</h2>
+        <p>Not sure where to start? We've created an easy process to find the pet you want. <a href="#">Click here to start it</a></p>
+        <div class="row">
+
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://goo.gl/0sX3jq">
+                </div>
+                <div class="thumbnail">
+                    <img src="http://goo.gl/an2HXY">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://goo.gl/Av1pac">
+                </div>
+                <div class="thumbnail">
+                    <img src="http://goo.gl/vw43v1">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://goo.gl/0Kd7UO">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="learn-more">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3></h3>
+                <p>From apartments and rooms to treehouses and boats: stay in unique spaces in 192 countries.</p>
+                <p><a href="#">See how to travel on Airbnb</a></p>
+            </div>
+            <div class="col-md-4">
+                <h3>Questions</h3>
+                <p>Renting out your unused space could pay your bills or fund your next vacation.</p>
+                <p><a href="#">Learn more about hosting</a></p>
+            </div>
+            <div class="col-md-4">
+                <h3>Trust and Safety</h3>
+                <p>From Verified ID to our worldwide customer support team, we've got your back.</p>
+                <p><a href="#">Learn about trust at Airbnb</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 4/11/16
+ * Time: 2:40 PM
+ */
+?>
